6bb8adfeb7c005ea108b4ff24af1c1b42ec54ea6
nayak <khageswarnayak2000@gmail.com>
2024-04-22 14:56:02 +0530

    First Commit

---
+++ src/main/java/com/babu24/jenkinspipline/JenkinsPiplineApplication.java
@@ -0,0 +1,13 @@
+package com.babu24.jenkinspipline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPiplineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPiplineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/babu24/jenkinspipline/JenkinsPiplineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babu24.jenkinspipline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPiplineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
